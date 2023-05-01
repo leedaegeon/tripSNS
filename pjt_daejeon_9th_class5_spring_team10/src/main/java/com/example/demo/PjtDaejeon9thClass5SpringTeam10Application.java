b69c7c70db9d6cd96fe1c7756a18f45b242ab032
이대건 <ldg031@naver.com>
2023-05-01 16:45:30 +0900

    Initial commit

---
+++ pjt_daejeon_9th_class5_spring_team10/src/main/java/com/example/demo/PjtDaejeon9thClass5SpringTeam10Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjtDaejeon9thClass5SpringTeam10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjtDaejeon9thClass5SpringTeam10Application.class, args);
+	}
+
+}
