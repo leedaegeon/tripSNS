7ca79d5c23f09b47393273a68a09ec0050d44fe1
sylee723 <seoyeonlee0723@gmail.com>
2023-05-02 17:14:14 +0900

    add: notice board CRUD

--- pjt_daejeon_9th_class5_spring_team10/src/main/java/com/ssafy/board/controller/NoticeBoardController.java
+++ pjt_daejeon_9th_class5_spring_team10/src/main/java/com/ssafy/board/controller/NoticeBoardController.java
@@ -3,8 +3,12 @@ package com.ssafy.board.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +35,20 @@ public class NoticeBoardController {
 	@GetMapping("/{noticeNo}")
 	public NoticeBoardDto getArticle(@PathVariable("noticeNo") int noticeNo) throws Exception {
 		return service.getArticle(noticeNo);
+	}
+	
+	@PostMapping
+	public void addBook(@RequestBody NoticeBoardDto article) throws Exception {
+		service.writeArticle(article);
+	}
+	
+	@PutMapping
+	public void modify(@RequestBody NoticeBoardDto article) throws Exception {
+		service.modifyArticle(article);
+	}
+	
+	@DeleteMapping("/{noticeNo}")
+	public void removeBook(@PathVariable("noticeNo") int noticeNo) throws Exception {
+		service.deleteArticle(noticeNo);
 	}
 }
