74c8bca98339a9b8b78fb2ce2b9149de05793825
sylee723 <seoyeonlee0723@gmail.com>
2023-05-17 17:52:09 +0900

    swagger

--- pjt_daejeon_9th_class5_spring_team10/src/main/java/com/ssafy/board/controller/NoticeBoardController.java
+++ pjt_daejeon_9th_class5_spring_team10/src/main/java/com/ssafy/board/controller/NoticeBoardController.java
@@ -15,8 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.board.model.NoticeBoardDto;
 import com.ssafy.board.model.service.NoticeBoardService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/notice")
+@Api("게시판 컨트롤러  API V1")
 @CrossOrigin("*")
 public class NoticeBoardController {
 
@@ -28,10 +32,12 @@ public class NoticeBoardController {
 	}
 	
 	@GetMapping
+	@ApiOperation(value = "게시판 글목록", notes = "모든 게시글의 정보를 반환한다.", response = List.class)
 	public List<NoticeBoardDto> getArticleList() throws Exception {
 		return service.listArticle();
 	}
 	
+	@ApiOperation(value = "게시판 글보기", notes = "글번호에 해당하는 게시글의 정보를 반환한다.", response = NoticeBoardDto.class)
 	@GetMapping("/{noticeNo}")
 	public NoticeBoardDto getArticle(@PathVariable("noticeNo") int noticeNo) throws Exception {
 		return service.getArticle(noticeNo);
--- pjt_daejeon_9th_class5_spring_team10/src/main/java/com/ssafy/member/controller/MemberController.java
+++ pjt_daejeon_9th_class5_spring_team10/src/main/java/com/ssafy/member/controller/MemberController.java
@@ -7,16 +7,15 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.ibatis.annotations.Delete;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.member.model.MemberDto;
 import com.ssafy.member.model.service.MemberService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/user")
